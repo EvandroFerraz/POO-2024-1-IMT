ff3adc583880bb24f027d91ce6427d0db5597c3e
Evandro Ferraz <ferraz.evandro@outlook.com>
2024-02-25 01:16:09 -0300

    Create Exerc4.java

---
+++ Semana02 - Exercícios sobre Algoritmos de Entrada e Saída/Exerc4.java
@@ -0,0 +1,11 @@
+import javax.swing.JOptionPane;
+
+public class Exerc4{
+  public static void main(String[] args){
+        // Ler o numero
+        int numero = Integer.parseInt(JOptionPane.showInputDialog("Digite um número: "));
+        // Imprimir o antecessor
+        int antecessor = numero - 1;
+        JOptionPane.showMessageDialog(null, "O antecessor é " + antecessor);
+  }
+}
