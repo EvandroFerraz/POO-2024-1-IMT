a00337020e9806fe0d1ccb5d62eaa351716e9bdb
Evandro Ferraz <ferraz.evandro@outlook.com>
2024-03-13 01:20:48 -0300

    Update Exercicio01.java

--- Semana04 - Laços de Repetição/Exercicio01.java
+++ Semana04 - Laços de Repetição/Exercicio01.java
@@ -1,1 +1,13 @@
+import javax.swing.JOptionPane;
+
+public class Exercicio01 {
+    public static void main(String[] args) {
+        // Enunciado: Escreva um programa que exibe os primeiros 100 números naturais. Faça versões usando for, while e do/while.
+        String output = "Primeiros 100 números naturais (usando for):\n";
+        for (int i = 0; i < 100; i++) {
+            output += i + " ";
+        }
+        JOptionPane.showMessageDialog(null, output);
+    }
+}
 
