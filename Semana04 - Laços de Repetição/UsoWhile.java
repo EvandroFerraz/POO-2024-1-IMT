b1c08f677be5eca0deca934d99d240f66efe3ab5
Evandro Ferraz <ferraz.evandro@outlook.com>
2024-03-14 16:29:52 -0300

    Update UsoWhile.java

--- Semana04 - Laços de Repetição/UsoWhile.java
+++ Semana04 - Laços de Repetição/UsoWhile.java
@@ -1,43 +1,35 @@
 import javax.swing.JOptionPane;
 
-public class UsoWhile {
+public class teste {
     public static void main(String args[]){
-        int opcao = Integer.parseInt(
-            JOptionPane.showInputDialog("1- Soma\n 2- Subtracao\n" +
-            "3- Multiplicacao\n4- Divisao\n0 - Sair")
-        );
+        boolean sairDoWhile = false;
 
-        while(opcao != 0){ 
-            double numero1 = Double.parseDouble(
-                JOptionPane.showInputDialog("Digite o primeiro número")
-            );
-            double numero2 = Double.parseDouble(
-                JOptionPane.showInputDialog("Digite o segundo número")
-            );
+        while(sairDoWhile == false){ 
+            int opcao = Integer.parseInt(JOptionPane.showInputDialog("1- Soma\n 2- Subtracao\n3- Multiplicacao\n4- Divisao\n0 - Sair"));
 
-            switch (opcao){
-                case 1:
-                    JOptionPane.showMessageDialog(null,numero1+numero2);
-                    break;
-                case 2:
-                    JOptionPane.showMessageDialog(null,numero1-numero2);
-                    break;
-                case 3:
-                    JOptionPane.showMessageDialog(null,numero1*numero2);
-                    break;
-                case 4:
-                    JOptionPane.showMessageDialog(null,numero1/numero2);
-                    break;
-                default:
-                    JOptionPane.showMessageDialog(null,"Opção inválida");
-                    opcao = 0;
-                    break;
+            if(opcao == 0){
+                sairDoWhile = true;
+            }else{ // opcao != 0
+                double numero1 = Double.parseDouble(JOptionPane.showInputDialog("Digite o primeiro número"));
+                double numero2 = Double.parseDouble(JOptionPane.showInputDialog("Digite o segundo número"));
+                switch (opcao){
+                    case 1:
+                        JOptionPane.showMessageDialog(null,numero1+numero2);
+                        break;
+                    case 2:
+                        JOptionPane.showMessageDialog(null,numero1-numero2);
+                        break;
+                    case 3:
+                        JOptionPane.showMessageDialog(null,numero1*numero2);
+                        break;
+                    case 4:
+                        JOptionPane.showMessageDialog(null,numero1/numero2);
+                        break;
+                    default:
+                        JOptionPane.showMessageDialog(null,"Opção inválida");
+                        break;
+                }
             }
-
-            opcao = Integer.parseInt(
-                JOptionPane.showInputDialog("1- Soma\n 2- Subtracao\n" +
-                "3- Multiplicacao\n4- Divisao\n0 - Sair")
-            );
         }
     }
 }
