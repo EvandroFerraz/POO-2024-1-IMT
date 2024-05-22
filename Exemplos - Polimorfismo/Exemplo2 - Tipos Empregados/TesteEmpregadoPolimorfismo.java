36c809e0ca31f13338c9ec0a2c691044f089b3d7
Evandro Ferraz <ferraz.evandro@outlook.com>
2024-05-22 17:42:22 -0300

    Update TesteEmpregadoPolimorfismo.java

--- Exemplos - Polimorfismo/Exemplo2 - Tipos Empregados/TesteEmpregadoPolimorfismo.java
+++ Exemplos - Polimorfismo/Exemplo2 - Tipos Empregados/TesteEmpregadoPolimorfismo.java
@@ -1,5 +1,7 @@
 public class TesteEmpregadoPolimorfismo {
     public static void main(String[] args){
+        // variáveis polimórficas e1, e2, e3, e4, e5
+        // cada uma atribuida a um modelo de classe de um empregado específico
         Empregado e1 = new Empregado(1000); //tipo 1 = salario
         Empregado e2 = new EmpregadoComissionado(1000,500); //tipo2 = salario e comissao
         Empregado e3 = new EmpregadoBonificado(1000,500,300); //tipo3 = salario, comissao e bonus
@@ -10,7 +12,7 @@ public class TesteEmpregadoPolimorfismo {
        System.out.println("Empregado Comissionado: " + e2.calculoSalario());
        System.out.println("Empregado Bonificado: " + e3.calculoSalario());
        System.out.println("Empregado Horista: " + e4.calculoSalario());
-       System.out.println("Empregado Tarefista: " + e4.calculoSalario());
+       System.out.println("Empregado Tarefeiro: " + e5.calculoSalario());
 
 
 
